7385160dd0bbad6c2078185f7517cd665c30da53
Anthony <tosharus26@yandex.ru>
2022-10-04 16:54:11 +0400

    .

--- core/config.py
+++ core/config.py
@@ -3,12 +3,9 @@ from environs import Env
 env = Env()
 env.read_env()
 
-TTL = 30000  # time to live token
-JWT_secret = env.str('SECRET')
-JWT_algorithm = env.str('ALGORITHM')
+
 DATABASE_URL = env.str('DATABASE_URL')
-ENCRYPTION_SCHEMAS = env.str('ENCRYPTION_SCHEMAS')
-TYPE_TOKEN = env.str('TYPE_TOKEN')
 PORT = env.int('PORT')
 ROUT = env.str('ROUT')
 SECRET_TOKEN = env.str('SECRET_TOKEN')
+URL_NEWSLETTERS = env.str('URL_NEWSLETTERS')
